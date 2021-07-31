2d852dde4775db07f69fa0e682e78d0b4b7868d1
xiaoyou66 <xiaoyou2333@foxmail.com>
2021-07-31 11:40:55 +0800

    fix: 修复B站卡片的问题

--- index.js
+++ index.js
@@ -15,14 +15,19 @@ const keyCookie = 'bili_cookie'
 const keyBiliPerson = 'side_bili_card_person_info'
 // B站认证信息
 const keyBiliAuth = 'bili_show'
+const keySyncNow = 'plugins_bili_sync_now'
 
 // 注册一个定时任务，每20分钟更新一下B站信息
 cron.start('0 0/20 * * * ? *',function (){
-    let uid = tools.getSetting(keyUID)
-    // uid不为空才更新
-    if (uid){
-        // 定时把B站个人信息更新到数据库中
-        tools.setKey(keyBiliPerson,tools.getBiliPersonInfo(tools.getSetting(keyUID),tools.getSetting(keyCookie)))
+    updateBliInfo()
+})
+
+// 添加管理员路由
+router.registerAdminRouter("GET","",function (context){
+    if (updateBliInfo()){
+        router.response.ResponseCreated(context,{})
+    } else {
+        router.response.ResponseServerError(context,"更新失败")
     }
 })
 
@@ -55,5 +60,18 @@ widget.addSide("","index.html",function () {
 widget.addSetting("B站个人信息卡片",1,[
     {title:"B站uid",type: "input",key: keyUID},
     {title:"个人认证",type: "input",key: keyBiliAuth},
-    {title:"B站cookie",type: "text",key: keyCookie}
+    {title:"B站cookie",type: "text",key: keyCookie},
+    {title:"立即更新数据",type: "row",key: keySyncNow, default: "admin/plugins/sideBiliCard"}
 ])
+
+function updateBliInfo(){
+    let uid = tools.getSetting(keyUID)
+    // uid不为空才更新
+    if (uid){
+        // 定时把B站个人信息更新到数据库中
+        tools.setKey(keyBiliPerson,tools.getBiliPersonInfo(tools.getSetting(keyUID),tools.getSetting(keyCookie)))
+        return true
+    } else {
+        return false
+    }
+}
